f78dcab0e21e52e0348e551fdd47ed892a768ed7
ChrisHayo <46949634+ChrisHayo@users.noreply.github.com>
2022-02-28 08:17:43 -1000

    Add files via upload

--- PCA9685.py
+++ PCA9685.py
@@ -3,7 +3,6 @@
 import time
 import math
 import smbus
-# import pygame
 
 # ============================================================================
 # Raspi PCA9685 16-Channel PWM Servo Driver
@@ -16,7 +15,6 @@ class PCA9685:
   __SUBADR2            = 0x03
   __SUBADR3            = 0x04
   __MODE1              = 0x00
-  __MODE2              = 0x01
   __PRESCALE           = 0xFE
   __LED0_ON_L          = 0x06
   __LED0_ON_H          = 0x07
@@ -27,8 +25,7 @@ class PCA9685:
   __ALLLED_OFF_L       = 0xFC
   __ALLLED_OFF_H       = 0xFD
 
-
-  def __init__(self, address=0x40, debug=True):
+  def __init__(self, address=0x40, debug=False):
     self.bus = smbus.SMBus(1)
     self.address = address
     self.debug = debug
@@ -69,7 +66,6 @@ class PCA9685:
     self.write(self.__MODE1, oldmode)
     time.sleep(0.005)
     self.write(self.__MODE1, oldmode | 0x80)
-    self.write(self.__MODE2, 0x04)
 
   def setPWM(self, channel, on, off):
     "Sets a single PWM channel"
@@ -78,19 +74,25 @@ class PCA9685:
     self.write(self.__LED0_OFF_L+4*channel, off & 0xFF)
     self.write(self.__LED0_OFF_H+4*channel, off >> 8)
     if (self.debug):
-      print("channel: %d  LED_ON: %d LED_OFF: %d" % (self.__LED0_ON_L+4*channel,on,off))
-
+      print("channel: %d  LED_ON: %d LED_OFF: %d" % (channel,on,off))
+	  
   def setServoPulse(self, channel, pulse):
     "Sets the Servo Pulse,The PWM frequency must be 50HZ"
     pulse = pulse*4096/20000        #PWM frequency is 50HZ,the period is 20000us
     self.setPWM(channel, 0, int(pulse))
+
+if __name__=='__main__':
+ 
+  pwm = PCA9685(0x40, debug=False)
+  pwm.setPWMFreq(50)
+  while True:
+   # setServoPulse(2,2500)
+    for i in range(500,2500,10):  
+      pwm.setServoPulse(0,i)
+      pwm.setServoPulse(1,i)
+      time.sleep(0.02)     
     
-  def setRotationAngle(self, channel, Angle): 
-    if(Angle >= 0 and Angle <= 180):
-        temp = Angle * (2000 / 180) + 501
-        self.setServoPulse(channel, temp)
-    else:
-        print("Angle out of range")
-    
-  def exit_PCA9685(self):
-    self.write(self.__MODE2, 0x00)
+    for i in range(2500,500,-10):
+      pwm.setServoPulse(0,i)
+      pwm.setServoPulse(1,i)
+      time.sleep(0.02)  
